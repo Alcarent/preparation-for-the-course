23408b2d71281fbb130666c9d69c6149f1537331
Alcarent <144226431+Alcarent@users.noreply.github.com>
2024-10-03 12:40:57 +0300

    create class

---
+++ task 0/Class_Problem.py
@@ -0,0 +1,6 @@
+from ortools.init.python import init
+from ortools.linear_solver import pywraplp
+
+
+class Problem:
+    pass
--- task 0/main.py
+++ task 0/main.py
@@ -1,5 +1,7 @@
 from ortools.init.python import init
 from ortools.linear_solver import pywraplp
+import Class_Problem
+import Class_Solver
 
 solver = pywraplp.Solver.CreateSolver("GLOP")
 infinity = solver.infinity()
@@ -9,14 +11,12 @@ x2_var = solver.NumVar(-infinity, infinity, "x2")
 x3_var = solver.NumVar(-3, infinity, "x3")
 x4_var = solver.NumVar(-infinity, infinity, "x4")
 
-
-constraint1 = solver.Constraint(-infinity, 5, "ct")
+constraint1 = solver.Constraint(-infinity, 5)
 constraint1.SetCoefficient(x1_var, 1)
 constraint1.SetCoefficient(x2_var, -1)
 constraint1.SetCoefficient(x3_var, -3)
 
-
-constraint2 = solver.Constraint(10, infinity, "ct")
+constraint2 = solver.Constraint(10, infinity)
 constraint2.SetCoefficient(x1_var, 2)
 constraint2.SetCoefficient(x2_var, -3)
 constraint2.SetCoefficient(x3_var, -7)
@@ -26,7 +26,6 @@ constraint3 = solver.Constraint(60, 60, "ct")
 constraint3.SetCoefficient(x1_var, 2)
 constraint3.SetCoefficient(x2_var, 8)
 constraint3.SetCoefficient(x3_var, 1)
-
 
 constraint4 = solver.Constraint(60, 60, "ct")
 constraint4.SetCoefficient(x1_var, 4)
@@ -44,5 +43,7 @@ print("Solution:")
 print("Objective value =", objective.Value())
 print("x1 =", x1_var.solution_value())
 print("x2 =", x2_var.solution_value())
+print("x3 =", x3_var.solution_value())
+print("x4 =", x4_var.solution_value())
 
 
